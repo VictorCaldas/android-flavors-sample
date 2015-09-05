7e2aff427bf58fef3f6bc36704a89e896ab6fe41
Victor Caldas <contatos.caldas@gmail.com>
2015-09-05 14:05:39 -0300

    commit flavors

--- app/src/main/java/com/github/flavors/sample/application/ui/activities/LoginActivity.java
+++ app/src/main/java/com/github/flavors/sample/application/ui/activities/LoginActivity.java
@@ -2,6 +2,8 @@ package com.github.flavors.sample.application.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.github.flavors.sample.R;
 
@@ -11,5 +13,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        Toast.makeText(this, R.string.login_message_test,Toast.LENGTH_LONG).show();
     }
 }
